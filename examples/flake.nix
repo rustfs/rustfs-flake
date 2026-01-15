aadad7a7684688aa2de6b18e13c62960cd5c279a
houseme <housemecn@gmail.com>
2026-01-15 15:32:09 +0800

    chore: add copyright headers
    - **Compliance**: Add Apache 2.0 copyright headers (Copyright 2024 RustFS Team) to all project files.

--- examples/flake.nix
+++ examples/flake.nix
@@ -1,3 +1,17 @@
+# Copyright 2024 RustFS Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 {
   description = "Example NixOS system using RustFS Flake";
 
@@ -18,11 +32,7 @@
             package = rustfs-flake.packages.${pkgs.system}.default;
 
             volumes = "/var/lib/rustfs/data";
-
-            # API Service on 9000
             address = "0.0.0.0:9000";
-
-            # Console Service on 9001
             consoleEnable = true;
             consoleAddress = "0.0.0.0:9001";
 
@@ -32,9 +42,7 @@
             logLevel = "info";
           };
 
-          # Open both API and Console ports in firewall
           networking.firewall.allowedTCPPorts = [ 9000 9001 ];
-
           system.stateVersion = "24.11";
         })
       ];
